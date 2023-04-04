3f34eb8b7fa459c682b707a5383893bd5c79b464
mitchxmas <mich.noel@gmail.com>
2023-04-04 10:00:59 +0800

    Project1 - Second push
    V2 - with stretch goals is underway :
    - 3 different levels defined : the level is input from dropdown menu
    - the board game changes per level
    - scoring function & rest functions now work 100%

--- mno_script.js
+++ mno_script.js
@@ -1,15 +1,19 @@
 // Global Variables and other declarations
-let boardArray = [0, 0, 0, 0, 0, 0, 0, 0, 0, 0];
-let numCards = 20;
+let boardArray = [];
+let numPairs = 4;
 const cardContainer = document.querySelector("#cardContainer");
 const player1Box = document.querySelector("#player1Box");
 const player2Box = document.querySelector("#player2Box");
+const player1ScoreBox = document.querySelector("#player1Score");
+const player2ScoreBox = document.querySelector("#player2Score");
 const gameMsgBox = document.querySelector("#gameMsgBox");
+const consolMsg = document.querySelector("#console");
 const cardVersoGraphics = "./images/playingcard.jpg";
 // "https://s3.amazonaws.com/images.penguinmagic.com/images/products/original/8007b.jpg";
 const playerNumber = () => (player === 1 ? "1" : "2");
 const whosTurn = () => `It's ${playerNumber()}'s turn`;
 
+let level = document.getElementById("levelMenu").value - 1;
 let player = 1;
 let cardsPlayed = [];
 let player1Score = 0;
@@ -18,105 +22,281 @@ let winner = 0;
 let gameWon = false;
 
 let cardDetails = [
-  {
-    pairIdx: 0,
-    level: "easy",
-    name: "bluey",
-    link: "./images/char-bluey@2x.png",
-    // link: "https://www.bluey.tv/wp-content/uploads/2019/04/char-bluey@2x.png",
-  },
-  {
-    pairIdx: 1,
-    level: "easy",
-    name: "bingo",
-    link: "./images/Bingo_dance-2x.png",
-    // link: "https://www.bluey.tv/wp-content/uploads/2019/05/Bingo_dance-2x.png",
-  },
-  {
-    pairIdx: 2,
-    level: "easy",
-    name: "bandit",
-    link: "./images/char-bandit@2x.png",
-    // link: "https://www.bluey.tv/wp-content/uploads/2019/04/char-bandit@2x.png",
-  },
-  {
-    pairIdx: 3,
-    level: "easy",
-    name: "chilli",
-    link: "./images/char-chilli@2x.png",
-    // link: "https://www.bluey.tv/wp-content/uploads/2019/04/char-chilli@2x.png",
-  },
-  {
-    pairIdx: 4,
-    level: "easy",
-    name: "muffin",
-    link: "./images/Muffin-testshadow.png",
-    // link: "https://www.bluey.tv/wp-content/uploads/2019/07/Muffin-testshadow.png",
-  },
-  {
-    pairIdx: 5,
-    level: "easy",
-    name: "jack",
-    link: "./images/Jack_Army-1.png",
-    // link: "https://www.bluey.tv/wp-content/uploads/2019/04/Jack_Army-1.png",
-  },
-  {
-    pairIdx: 6,
-    level: "easy",
-    name: "rusty",
-    link: "./images/Rusty-1.png",
-    // link: "https://www.bluey.tv/wp-content/uploads/2019/06/Rusty-1.png",
-  },
-  {
-    pairIdx: 7,
-    level: "easy",
-    name: "lucky",
-    link: "./images/Lucky.png",
-    // link: "https://www.bluey.tv/wp-content/uploads/2019/06/Lucky.png",
-  },
-  {
-    pairIdx: 8,
-    level: "easy",
-    name: "mackenzie",
-    link: "./images/Mackenzie-newshadow.png",
-    // link: "https://www.bluey.tv/wp-content/uploads/2019/06/Mackenzie-newshadow.png",
-  },
-  {
-    pairIdx: 9,
-    level: "easy",
-    name: "indy",
-    link: "./images/char-indy@2x.png",
-    // link: "https://www.bluey.tv/wp-content/uploads/2019/04/char-indy@2x.png",
-  },
-  {
-    pairIdx: 10,
-    level: "easy",
-    name: "coco",
-    link: "./images/coco@2x.png",
-    // link: "https://www.bluey.tv/wp-content/uploads/2019/04/coco@2x.png",
-  },
+  // Easy Level Cards
+  [
+    {
+      pairIdx: 0,
+      level: "easy",
+      name: "bluey",
+      link: "./images/wildCard.png",
+      // link: "./images/char-bluey.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/04/char-bluey.png",
+    },
+    {
+      pairIdx: 1,
+      level: "easy",
+      name: "bingo",
+      link: "./images/Bingo_dance.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/05/Bingo_dance-2x.png",
+    },
+    {
+      pairIdx: 2,
+      level: "easy",
+      name: "bandit",
+      link: "./images/char-bandit.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/04/char-bandit.png",
+    },
+    {
+      pairIdx: 3,
+      level: "easy",
+      name: "chilli",
+      link: "./images/char-chilli.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/04/char-chilli.png",
+    },
+    {
+      pairIdx: 4,
+      level: "easy",
+      name: "muffin",
+      link: "./images/Muffin-testshadow.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/07/Muffin-testshadow.png",
+    },
+    {
+      pairIdx: 5,
+      level: "easy",
+      name: "jack",
+      link: "./images/Jack_Army-1.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/04/Jack_Army-1.png",
+    },
+    {
+      pairIdx: 6,
+      level: "easy",
+      name: "rusty",
+      link: "./images/Rusty-1.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/06/Rusty-1.png",
+    },
+    {
+      pairIdx: 7,
+      level: "easy",
+      name: "lucky",
+      link: "./images/Lucky.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/06/Lucky.png",
+    },
+    {
+      pairIdx: 8,
+      level: "easy",
+      name: "mackenzie",
+      link: "./images/Mackenzie-newshadow.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/06/Mackenzie-newshadow.png",
+    },
+    {
+      pairIdx: 9,
+      level: "easy",
+      name: "indy",
+      link: "./images/char-indy.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/04/char-indy.png",
+    },
+    {
+      pairIdx: 10,
+      level: "easy",
+      name: "coco",
+      link: "./images/coco.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/04/coco.png",
+    },
+  ],
+  // Medium Level Cards
+  [
+    {
+      pairIdx: 0,
+      level: "medium",
+      name: "bluey1",
+      link: "./images/char-bluey.png",
+    },
+    {
+      pairIdx: 1,
+      level: "medium",
+      name: "bluey2",
+      link: "./images/char-bluey2.png",
+    },
+    {
+      pairIdx: 2,
+      level: "medium",
+      name: "bingo",
+      link: "./images/Bingo_dance.png",
+    },
+    {
+      pairIdx: 3,
+      level: "medium",
+      name: "bingo2",
+      link: "./images/Bingo_dance2.png",
+    },
+    {
+      pairIdx: 4,
+      level: "medium",
+      name: "bandit",
+      link: "./images/char-bandit.png",
+    },
+    {
+      pairIdx: 5,
+      level: "medium",
+      name: "bandit2",
+      link: "./images/char-bandit2.png",
+    },
+    {
+      pairIdx: 6,
+      level: "medium",
+      name: "chilly",
+      link: "./images/char-chilli.png",
+    },
+    {
+      pairIdx: 7,
+      level: "medium",
+      name: "chilli2",
+      link: "./images/char-chilli2.png",
+    },
+    {
+      pairIdx: 8,
+      level: "medium",
+      name: "indy",
+      link: "./images/char-indy.png",
+    },
+    {
+      pairIdx: 9,
+      level: "medium",
+      name: "indy2",
+      link: "./images/char-indy2.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/04/char-indy.png",
+    },
+    {
+      pairIdx: 10,
+      level: "medium",
+      name: "coco",
+      link: "./images/coco.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/04/coco.png",
+    },
+    {
+      pairIdx: 11,
+      level: "medium",
+      name: "coco2",
+      link: "./images/coco2.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/04/coco.png",
+    },
+    {
+      pairIdx: 12,
+      level: "medium",
+      name: "rusty",
+      link: "./images/Rusty-1.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/04/coco.png",
+    },
+    {
+      pairIdx: 13,
+      level: "medium",
+      name: "rusty2",
+      link: "./images/Rusty-2.png",
+      // link: "https://www.bluey.tv/wp-content/uploads/2019/04/coco.png",
+    },
+  ],
+  [
+    {
+      pairIdx: 0,
+      level: "hard",
+      name: "wildcard",
+      link: "./images/wildCardRed.png",
+    },
+    {
+      pairIdx: 0,
+      level: "hard",
+      name: "geo1",
+      link: "./images/geo1.png",
+    },
+    {
+      pairIdx: 1,
+      level: "hard",
+      name: "geo2",
+      link: "./images/geo2.png",
+    },
+    {
+      pairIdx: 2,
+      level: "hard",
+      name: "geo3",
+      link: "./images/geo3.png",
+    },
+    {
+      pairIdx: 3,
+      level: "hard",
+      name: "geo4",
+      link: "./images/geo4.png",
+    },
+    {
+      pairIdx: 4,
+      level: "hard",
+      name: "geo5",
+      link: "./images/geo5.png",
+    },
+    {
+      pairIdx: 5,
+      level: "hard",
+      name: "geo6",
+      link: "./images/geo6.png",
+    },
+    {
+      pairIdx: 6,
+      level: "hard",
+      name: "geo7",
+      link: "./images/geo7.png",
+    },
+    {
+      pairIdx: 7,
+      level: "hard",
+      name: "geo8",
+      link: "./images/geo8.png",
+    },
+    {
+      pairIdx: 8,
+      level: "hard",
+      name: "geo7",
+      link: "./images/geo7.png",
+    },
+    {
+      pairIdx: 9,
+      level: "hard",
+      name: "geo8",
+      link: "./images/geo8.png",
+    },
+    {
+      pairIdx: 10,
+      level: "hard",
+      name: "geo8",
+      link: "./images/geo9.png",
+    },
+  ],
 ];
 
-createArray(numCards);
+createArray(numPairs);
 createTiles();
 // Functions
 //TEMP FUNCTION ACTING AS A CONSOLE LOG
 function showStuff() {
-  const showMsg = `cardplayed length: ${cardsPlayed.length} & array: ${cardsPlayed}`;
-  document.querySelector("#game--title").innerText = showMsg;
-  // cardDetails[cellNme].link;
+  // const e = document.getElementById("levelMenu");
+  // const value = e.value;
+
+  consolMsg.innerText = `Level is : ${level}`;
 }
 
 function playerClickCard(event) {
   const clickedCard = event.target;
   const clickedCardPair = parseInt(clickedCard.getAttribute("pair"));
 
-  clickedCard.src = cardDetails[clickedCardPair].link;
-  clickedCard.classList.add("clicked");
-  cardsPlayed.push(clickedCardPair);
-
-  showStuff();
-  setTimeout(checkResults, 500);
+  if (
+    clickedCard.classList.contains("clicked") ||
+    clickedCard.classList.contains("hide")
+  ) {
+    return;
+  } else {
+    clickedCard.src = cardDetails[level][clickedCardPair].link;
+    clickedCard.classList.add("clicked");
+    cardsPlayed.push(clickedCardPair);
+    setTimeout(checkResults, 500);
+  }
 }
 
 function checkResults() {
@@ -126,53 +306,51 @@ function checkResults() {
       //cards are identical, a pair is found
       gameMsgBox.innerText = `Congrats player ${playerNumber()}, you found a pair! `;
       window.alert(`Congrats player ${playerNumber()}, you found a pair!`);
+      changeScore();
       hidePairs();
 
-      player1Score += 1;
+      gameMsgBox.innerText = whosTurn();
     } else {
       window.alert(`Sorry player ${playerNumber()}, bad luck`);
       turnClickedCardsOver();
+      changePlayerFn();
     }
-
     cardsPlayed = [];
+    showStuff();
   }
 
-  if (player1Score + player2Score === 0.5 * numCards) {
-    gameMsgBox.innerHTML = winningMsg();
+  if (player1Score + player2Score === numPairs) {
+    gameMsgBox.innerText = winningMsg();
     gamePlaying = false;
     return;
   }
 
-  let gameTie = (player1Score === player2Score) === 0.5 * numCards;
+  let gameTie = (player1Score === player2Score) === numPairs;
   if (gameTie) {
     winner = "T";
-    gameMsgBox.innerHTML = winningMsg(); //`Game ended in a draw!`;
+    gameMsgBox.innerText = winningMsg(); //`Game ended in a draw!`;
     gamePlaying = false;
     return;
   }
-
-  // changePlayerFn();
 }
 
-// function changePlayerFn() {
-//   player = player * -1;
-//   const player1Box = document.querySelector("#player1Box");
-//   const player2Box = document.querySelector("#player2Box");
+function changePlayerFn() {
+  player = player * -1;
 
-//   if (player === 1) {
-//     player1Box.classList.remove("hide");
-//     player2Box.classList.add("hide");
-//   }
-//   if (player === -1) {
-//     player1Box.classList.remove("hide");
-//     player2Box.classList.add("hide");
-//   }
+  if (player === 1) {
+    player1Box.classList.remove("hide");
+    player2Box.classList.add("hide");
+  }
+  if (player === -1) {
+    player2Box.classList.remove("hide");
+    player1Box.classList.add("hide");
+  }
 
-//   gameMsgBox.innerHTML = whosTurn();
-// }
+  gameMsgBox.innerHTML = whosTurn();
+}
 
-function createArray(numCards) {
-  for (let i = 0; i < numCards; i++) {
+function createArray(numPairs) {
+  for (let i = 0; i < 2 * numPairs; i++) {
     boardArray[i] = (i - (i % 2)) * 0.5;
   }
   // Shuffle the array using the Fisher-Yates algorithm
@@ -184,14 +362,16 @@ function createArray(numCards) {
 
 function createTiles() {
   for (let i = 0; i < boardArray.length; i++) {
-    var newCardImg = document.createElement("img");
+    const newCardImg = document.createElement("img");
     newCardImg.id = `img${i}`;
     newCardImg.classList.add("cardBox");
     newCardImg.setAttribute("pair", `${boardArray[i]}`);
-    newCardImg.setAttribute("name", cardDetails[boardArray[i]].name);
-
+    newCardImg.setAttribute("name", cardDetails[level][boardArray[i]].name);
     newCardImg.src = cardVersoGraphics;
     cardContainer.appendChild(newCardImg);
+    newCardImg.addEventListener("click", playerClickCard);
+    // newCardImg.draggable = "true";
+    // newCardImg.ondragstart = "drag(event)";
   }
   //still need to asign a pictures. Best to create an object (card num, pic details, value(active or note)??
 }
@@ -199,19 +379,42 @@ function createTiles() {
 function resetGameFn() {
   //turn all cards back over, reshuffle everything
   //set pair count to 0 for each player
+  player = 1;
+  player1Score = 0;
+  player2Score = 0;
 
-  createArray(numCards);
+  const cardContainer = document.getElementById("cardContainer");
+  while (cardContainer.firstChild) {
+    cardContainer.removeChild(cardContainer.firstChild);
+  }
+
+  player1ScoreBox.innerText = 0;
+  player2ScoreBox.innerText = 0;
+  gameMsgBox.innerText = "Player 1 starts: click two cards on the board!";
+  player1Box.classList.remove("hide");
+  player2Box.classList.add("hide");
+  boardArray = [];
+  cardsPlayed = [];
+  createArray(numPairs);
   createTiles();
 }
 
-function changeDisplay() {
-  //add 1 to the players score
-  //hide the collected cards
+function changeScore() {
+  //if a pair is found add 1 to the players score
+  if (player === 1) {
+    player1Score += 1;
+  } else {
+    player2Score += 1;
+  }
+  document.querySelector("#player1Score").innerText = player1Score;
+  document.querySelector("#player2Score").innerText = player2Score;
+  return;
 }
 
 function turnClickedCardsOver() {
   for (const item of document.querySelectorAll(".clicked")) {
     item.src = cardVersoGraphics;
+    item.classList.remove("clicked");
   }
 }
 
@@ -223,19 +426,21 @@ function hidePairs() {
 }
 
 function winningMsg() {
-  if (winner === 1) {
-    return `Player 1 has won. Congrats`;
-  } else if (winner === -1) {
-    return `Player 2 has won. Congrats`;
-  } else if (winner === "T") {
-    return `It was a Tie.`;
+  if (player1Score > player2Score) {
+    return "Player 1 has won";
+  } else if (player1Score < player2Score) {
+    return "Player 2 has won";
+  } else {
+    return "It's a tie";
   }
 }
 
-//EVent listeners
-// document.querySelector("#img0").addEventListener("click", playerClickCard);
+function selectLevel() {
+  level = document.getElementById("levelMenu").value - 1;
+  consolMsg.innerText = `Level is : ${level}`;
+}
 
-document
-  .querySelectorAll(".cardBox")
-  .forEach((card) => card.addEventListener("click", playerClickCard));
-// document.querySelector(".gameReset").addEventListener("click", resetGameFn);
+//Event Listeners
+
+document.querySelector("#gameReset").addEventListener("click", resetGameFn);
+document.querySelector("#levelMenu").addEventListener("change", selectLevel);
